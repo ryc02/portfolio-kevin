2e158a5d2b5678917ebcd1c13292d30a20076dbe
Kevin <kevin@example.com>
2026-06-01 23:31:57 -0300

    revert objectFit to contain for Bahira

--- src/components/ProjectIndex.jsx
+++ src/components/ProjectIndex.jsx
@@ -4,7 +4,7 @@ import { useLanguage } from '../contexts/LanguageContext';
 
 const indexData = [
   { id: "01", title: "VITTA STUDIO", image: "/assets/capa-vitta-studio.webp" },
-  { id: "02", title: "BAHIRA", image: "/assets/capa-bahira-nova.png", objectPosition: "right", objectFit: "cover" },
+  { id: "02", title: "BAHIRA", image: "/assets/capa-bahira-nova.png", objectPosition: "center", objectFit: "contain" },
   { id: "03", title: "AMBEV", image: "/assets/capa-ambev.webp" },
   { id: "04", title: "WOODORA", image: "/assets/capa-woodora.webp" },
   { id: "05", title: "L.A.N.S", image: "/assets/capa-lans.webp" }
